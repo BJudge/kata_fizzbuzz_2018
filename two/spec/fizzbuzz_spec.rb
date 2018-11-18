b13c97a57d6a6564b9f4cd468a9a00a836777b60
BartJudge <BartJudge@gmail.com>
2018-11-18 02:04:54 +0000

    second attempt

---
+++ two/spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'should say fizz' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'should say buzz' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'should say fizzbuzz' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'should say 4' do
+    expect(fizzbuzz(4)). to eq 4
+  end
+end
